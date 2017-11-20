6b1daaba8d0aff3f90524c6960429b1b56a2ca6b
Atif Butt <Atif Butt>
2017-11-20 06:22:28 -0800

    Add project files.

---
+++ MiniMaxSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniMaxSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+            long[] inputArray = Array.ConvertAll(input, long.Parse);
+
+            long[] MinMax = ComputeMinMax(inputArray);
+            Console.WriteLine($"{MinMax.Min()} {MinMax.Max()}");
+            Console.ReadKey();
+        }
+
+        private static long[] ComputeMinMax(long[] inputArray)
+        {
+            long[] sum = new long[inputArray.Length];
+           
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                for (int j = 0; j < inputArray.Length; j++)
+                {
+                    if (j != i)
+                    {
+                        sum[i] += inputArray[j];
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
